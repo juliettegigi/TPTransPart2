bc54f454a5433d8f1f596cd27ea3d1883cd70857
Santiago <santigonzalez555@gmail.com>
2023-05-25 19:59:13 -0300

    main +

--- TPTransPart2/src/tptranspart2/TPTransPart2.java
+++ TPTransPart2/src/tptranspart2/TPTransPart2.java
@@ -24,74 +24,75 @@ public class TPTransPart2 {
      */
     public static void main(String[] args) {
 
-        for(Inscripcion inscrip:InscripcionData.obtenerInscripcionesPorAlumno(3)){
-           System.out.println(inscrip.getAlumno().getNombre()+" "+inscrip.getMateria().getNombre());
-       }
-       
-//------------------AGREGO ALUMNO------------------
+//-----------------------------AGREGO ALUMNO-----------------------------
         //AlumnoData.guardarAlumno(new Alumno(11122233, "Garro", "Juan", LocalDate.of(2000, 5, 22), true));
         
-//--------------BUSCO ALUMNO POR ID----------------
+//-------------------------BUSCO ALUMNO POR ID---------------------------
         //System.out.println(AlumnoData.buscarAlumno(3));
         
-//-------------BUSCO ALUMNO POR DNI----------------   
-        //System.out.println(AlumnoData.buscarAlumnoPorDni(11122233));
+//------------------------BUSCO ALUMNO POR DNI---------------------------   
+        //System.out.println(AlumnoData.buscarAlumnoPorDni(38959654));
         
-//------------------LISTO ALUMNO-------------------
+//-----------------------------LISTO ALUMNO------------------------------
         //System.out.println("Alumnos: \n" + AlumnoData.listarAlumnos());
         
-//----------------MODIFICO ALUMNO------------------  
+//---------------------------MODIFICO ALUMNO----------------------------- 
         //AlumnoData.modificarAlumno(new Alumno(4, 30956879, "Messi", "Lionel", LocalDate.of(1987, 6, 24), true));
         
-//----------------DAR BAJA ALUMNO------------------ BORRADO LÓGICO
+//---------------------------DAR BAJA ALUMNO----------------------------- BORRADO LÓGICO
         //AlumnoData.darBajaAlumno(2);
         
-//-----------------ELIMINO ALUMNO------------------ BORRADO DEFINITIVO
-        //AlumnoData.eliminarAlumno(6);
+//----------------------------ELIMINO ALUMNO----------------------------- BORRADO DEFINITIVO
+        //AlumnoData.eliminarAlumno(5);
         
+//__________________________________________________________________________________________________________________
         
-//-------------------AGREGO MATERIA----------------
+//------------------------------AGREGO MATERIA---------------------------
         //MateriaData.guardarMateria(new Materia("Sociologia", 2, true));
         
-//------------------LISTO MATERIAS-----------------       
+//-----------------------------LISTO MATERIAS----------------------------       
         //System.out.println("Materias: \n" + MateriaData.listarMaterias());
         
-//---------------BUSCO MATERIA POR ID--------------
+//--------------------------BUSCO MATERIA POR ID-------------------------
         //System.out.println(MateriaData.buscarMateria(3));
         
-//----------------MODIFICO MATERIA-----------------   
-        //MateriaData.modificarMateria(new Materia(6, "Ingles II", 3, true));
+//---------------------------MODIFICO MATERIA----------------------------   
+        //MateriaData.modificarMateria(new Materia(5, "Ingles II", 3, true));
         
-//----------------DAR BAJA MATERIA----------------- BORRADO LÓGICO
+//---------------------------DAR BAJA MATERIA---------------------------- BORRADO LÓGICO
         //MateriaData.darBajaMaterias(6);
         
-//-----------------ELIMINO MATERIA----------------- BORRADO DEFINITIVO
+//----------------------------ELIMINO MATERIA---------------------------- BORRADO DEFINITIVO
         //MateriaData.eliminarMateria(6);
         
-        
-//----------------AGREGO INSCRIPCION---------------
+//__________________________________________________________________________________________________________________      
+
+//---------------------------AGREGO INSCRIPCION--------------------------
         //Alumno a = AlumnoData.buscarAlumno(1);
         //Materia m = MateriaData.buscarMateria(4);
         //InscripcionData.guardarInscripcion(new Inscripcion(a, 10, m));
         
-//----------------LISTO INSCRIPCIONES--------------
+//---------------------------LISTO INSCRIPCIONES-------------------------
         //System.out.println(InscripcionData.obtenerInscripciones());
         
-//-------------LISTO MATERIAS CURSADAS-------------
-        //InscripcionData.obtenerMateriasCursadas(3);                          ERROR
+//------------------------LISTO MATERIAS CURSADAS------------------------
+        //InscripcionData.obtenerMateriasCursadas(3);
         
-//-----------LISTO MATERIAS NO CURSADAS------------
-        //InscripcionData.obtenerMateriasNOCursadas(3);                        ERROR
+//----------------------LISTO MATERIAS NO CURSADAS-----------------------
+        //InscripcionData.obtenerMateriasNOCursadas(3);
         
-//----------LISTO INSCRIPCIONES X ALUMNO-----------
-
-//------------------ACTUALIZO NOTA-----------------
-        //InscripcionData.actualizarNota(2, 4, 9);
+//---------------------LISTO INSCRIPCIONES X ALUMNO----------------------
+        //for(Inscripcion inscrip:InscripcionData.obtenerInscripcionesPorAlumno(3)){
+           //System.out.println(inscrip.getAlumno().getNombre()+" "+inscrip.getMateria().getNombre());}
+       
+//-----------------------------ACTUALIZO NOTA----------------------------
+        //InscripcionData.actualizarNota(2, 4, 10);
         
-//--------------LISTO ALUMNOS X MATERIA------------
+//-------------------------LISTO ALUMNOS X MATERIA-----------------------
         //System.out.println(InscripcionData.obtenerAlumnosPorMateria(1));
         
-//---------------ELIMINO INSCRIPCION--------------- BORRADO DEFINITIVO
-        //InscripcionData.borrarInscripcionMateriaAlumno(1, 4);
+//--------------------------ELIMINO INSCRIPCION-------------------------- BORRADO DEFINITIVO
+        //InscripcionData.borrarInscripcionMateriaAlumno(2, 4);
+        
     }
 }
