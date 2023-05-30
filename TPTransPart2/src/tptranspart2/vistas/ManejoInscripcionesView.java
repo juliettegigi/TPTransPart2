c32d2f2553011e17734db8e61c52d4733c059ac8
PaulaPriotti <paaupriotti@gmail.com>
2023-05-29 21:36:49 -0300

    inscripcion

--- TPTransPart2/src/tptranspart2/vistas/ManejoInscripcionesView.java
+++ TPTransPart2/src/tptranspart2/vistas/ManejoInscripcionesView.java
@@ -5,19 +5,30 @@
  */
 package tptranspart2.vistas;
 
+import java.util.ArrayList;
+import javax.swing.table.DefaultTableModel;
+import tptranspart2.accesoADatos.AlumnoData;
+import tptranspart2.accesoADatos.InscripcionData;
+import tptranspart2.entidades.Alumno;
+
+
+
 /**
  *
  * @author julie
  */
 public class ManejoInscripcionesView extends javax.swing.JInternalFrame {
-
+    private DefaultTableModel modelo;
+    
     /**
      * Creates new form ManejoInscripcionesView
      */
     public ManejoInscripcionesView() {
         initComponents();
+        llenarColumna();
+        llenarTabla();
     }
-
+   
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -27,21 +38,225 @@ public class ManejoInscripcionesView extends javax.swing.JInternalFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jComboBoxAlumnos = new javax.swing.JComboBox<>();
+        jButton2 = new javax.swing.JButton();
+        jButtonSalir = new javax.swing.JButton();
+        jDesktopPane1 = new javax.swing.JDesktopPane();
+        jRadioButtonNoIns = new javax.swing.JRadioButton();
+        jRadioButtonIns = new javax.swing.JRadioButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTableT = new javax.swing.JTable();
+        jLabel3 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Formulario de inscripcion");
+
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setText("Alumno");
+
+        jComboBoxAlumnos.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jComboBoxAlumnosActionPerformed(evt);
+            }
+        });
+
+        jButton2.setText("Anular inscrpcion");
+
+        jButtonSalir.setText("Salir");
+        jButtonSalir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonSalirActionPerformed(evt);
+            }
+        });
+
+        jRadioButtonNoIns.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jRadioButtonNoIns.setText("No inscripto");
+        jRadioButtonNoIns.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jRadioButtonNoInsActionPerformed(evt);
+            }
+        });
+
+        jRadioButtonIns.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jRadioButtonIns.setText("Inscripto");
+        jRadioButtonIns.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jRadioButtonInsActionPerformed(evt);
+            }
+        });
+
+        jTableT.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {}
+            },
+            new String [] {
+
+            }
+        ));
+        jScrollPane1.setViewportView(jTableT);
+
+        jDesktopPane1.setLayer(jRadioButtonNoIns, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        jDesktopPane1.setLayer(jRadioButtonIns, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        jDesktopPane1.setLayer(jScrollPane1, javax.swing.JLayeredPane.DEFAULT_LAYER);
+
+        javax.swing.GroupLayout jDesktopPane1Layout = new javax.swing.GroupLayout(jDesktopPane1);
+        jDesktopPane1.setLayout(jDesktopPane1Layout);
+        jDesktopPane1Layout.setHorizontalGroup(
+            jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jDesktopPane1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 496, Short.MAX_VALUE))
+            .addGroup(jDesktopPane1Layout.createSequentialGroup()
+                .addGap(75, 75, 75)
+                .addComponent(jRadioButtonIns)
+                .addGap(107, 107, 107)
+                .addComponent(jRadioButtonNoIns)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jDesktopPane1Layout.setVerticalGroup(
+            jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jDesktopPane1Layout.createSequentialGroup()
+                .addGroup(jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jRadioButtonIns)
+                    .addComponent(jRadioButtonNoIns))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 223, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(42, Short.MAX_VALUE))
+        );
+
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel3.setText("Listado de materias");
+
+        jButton1.setText("Inscribir");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 731, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(124, 124, 124)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 287, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(73, 73, 73)
+                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 94, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(31, 31, 31)
+                        .addComponent(jComboBoxAlumnos, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(161, 161, 161)
+                        .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 173, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 6, Short.MAX_VALUE)
+                .addComponent(jDesktopPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(167, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(174, 174, 174)
+                .addComponent(jButton1)
+                .addGap(18, 18, 18)
+                .addComponent(jButton2)
+                .addGap(18, 18, 18)
+                .addComponent(jButtonSalir)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 482, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jComboBoxAlumnos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jDesktopPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton2)
+                    .addComponent(jButtonSalir)
+                    .addComponent(jButton1))
+                .addContainerGap(31, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButtonSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonSalirActionPerformed
+        //boton salir
+        dispose();
+    }//GEN-LAST:event_jButtonSalirActionPerformed
+   
+    private void jComboBoxAlumnosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBoxAlumnosActionPerformed
+        //lista de alumnos
+        AlumnoData ad= new AlumnoData();
+       for (Alumno a:ad.listarAlumnos()){
+           jComboBoxAlumnos.addItem(a);
+       }
+    }//GEN-LAST:event_jComboBoxAlumnosActionPerformed
 
+    private void jRadioButtonNoInsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButtonNoInsActionPerformed
+        // boton no Inscripto
+        llenarTabla();
+    }//GEN-LAST:event_jRadioButtonNoInsActionPerformed
+    
+    private void jRadioButtonInsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButtonInsActionPerformed
+        //boton Inscripto
+        llenarTabla();
+    }//GEN-LAST:event_jRadioButtonInsActionPerformed
+    private void llenarColumna(){
+           ArrayList<Object> columna=new ArrayList<>();
+           columna.add("ID");
+           columna.add("Alumno");
+           columna.add("Nota");
+
+           for(Object it:columna){
+               modelo.addColumn(it);
+           }
+           jTableT.setModel(modelo);
+       }
+    private void llenarTabla(){
+        borrarFilas();
+        Alumno elegida=(Alumno)jComboBoxAlumnos.getSelectedItem();
+        AlumnoData ad=new AlumnoData();
+        if(elegida!=null){
+            for(Alumno a:ad.listarAlumnos()){
+                if(a.isEstado()==true){
+                    modelo.addRow(new Object[](a.getIdAlumno(),a.getApellido(),a.isEstado()));
+                } else{
+                    modelo.addRow(new Object[](a.getIdAlumno(),a.getApellido(),a.isEstado()));
+                }
+            }
+        }
+        
+    }
+    public void borrarFilas(){
+        int a=modelo.getRowCount()-1;
+        for(int i=a;i>=0;i--){
+            modelo.removeRow(i);
+        }
+    }
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButtonSalir;
+    private javax.swing.JComboBox<Alumno> jComboBoxAlumnos;
+    private javax.swing.JDesktopPane jDesktopPane1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JRadioButton jRadioButtonIns;
+    private javax.swing.JRadioButton jRadioButtonNoIns;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTableT;
     // End of variables declaration//GEN-END:variables
 }
